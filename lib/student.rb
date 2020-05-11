bdaedbc9f79b49bacc2d3af29f95a7e459793357
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-05-11 15:07:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,8 +27,8 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
-  def self.create(name, grade)
-    s = Student.new(name, grade)
+  def self.create(name, grade, id)
+    s = Student.new(name, grade, id)
     s.save
     s 
   end 
