ddbc1095b948d8d4072db8a1cdf2ab1bcc05e5e6
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-05-11 14:29:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,16 @@
 class Student
+  
+  self.create_table
+    CREATE TABLE (
+      )
+  end 
+  
   attr_accessor :name, :grade
   attr_reader :id
   
   def initialize(name, grade, id=nil)
     @name, @grade, @id = name, grade, id 
   end
-  
-  
-  
+
 end
 
